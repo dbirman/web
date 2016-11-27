a7f1de57568f707138249475632cb57f1e98540d
Dan Birman <danbirman@gmail.com>
2016-11-27 22:41:13 +0100

    Added the individual trial section for DDM

--- learn/psych50_ddm.js
+++ learn/psych50_ddm.js
@@ -262,18 +262,180 @@ function drawPlot2() {
 ////////// BLOCK 6 /////////////
 ////////////////////////////////
 
+var out;
+var outputArea = document.getElementById("output6");
+var outputArea2 = document.getElementById("output62");
 
+var coh6 = Math.random();
+var dir6 = (Math.random()>0.5) ? 1 : -1;
+
+function textarea6(e) {
+	var key = window.event.keyCode;
+	if (key===13) {
+		e.preventDefault();
+		eval(document.getElementById('textarea6').value);
+		outputArea.value = "out = " + out;
+	}
+}
+function textarea62(e) {
+	var key = window.event.keyCode;
+	if (key===13) {
+		e.preventDefault();
+		try {
+			eval(document.getElementById('textarea62').value);
+			respL = leftResponse(coh6,dir6);
+			respR = rightResponse(coh6,dir6);
+			var dirs = ['Left','','Right'];
+			outputArea2.innerHTML = "Coherence = " + (Math.round(coh6*100)/100) + "&#13;&#10;Direction = " + dirs[dir6+1] + "&#13;&#10;Left response = " + respL + "&#13;&#10;Right response = " + respR;
+			done6 = true;
+			document.getElementById("continue").style.display="";
+			document.getElementById("end6").style.display="";
+		} catch (error) {
+			outputArea2.innerHTML = "Your code produced an error: " + "&#13;&#10;" + error;
+		}
+	}
+}
+
+function bonus1() {
+	document.getElementById("bonus1").style.display="";
+}
+
+////////////////////////////////
+////////// BLOCK 7 /////////////
+////////////////////////////////
+var outputArea7 = document.getElementById("output7");
+
+function textarea7(e) {
+	var key = window.event.keyCode;
+	if (key===13) {
+		e.preventDefault();
+		try {
+			eval(document.getElementById('textarea7').value);
+			var l = Math.random()*10;
+			var r = Math.random()*10;
+			var acc = Math.random()*100-50;
+			var true_na = acc + l-r;
+			var na = accumulateDDM(l,r,acc);
+			var na_correct = true_na==na;
+			var threshold = 20;
+			var dir = atThreshold(true_na,threshold);
+			var true_dir = (true_na>threshold) ? 1 : ((true_na<(-threshold)) ? -1 : 0);
+			var dir_correct = dir==true_dir;
+			outputArea7.innerHTML = "You accumulated: " + na + " we got: " + true_na + " your result is: " + na_correct + "&#13;&#10;You said the dir was: " + dir + " we got: " + true_dir + " your result is: " + dir_correct;
+			if (na_correct && dir_correct) {
+				done7 = true;
+				document.getElementById("continue").style.display="";
+				document.getElementById("end7").style.display="";	
+				resetSample7();
+			}
+		} catch (error) {
+			outputArea7.innerHTML = "Your code produced an error: " + "&#13;&#10;" + error;
+		}
+	}
+}
+
+var canvas_sample2 = document.getElementById("sample2");
+var ctx_sample2 = canvas_sample2.getContext("2d");
+
+var tick7;
+
+var dots7 = initDots(400,100,100,0,0);
+var coherence7 = 0.75;
+var dir7 = 1;
+
+
+function drawMotionSample7() {
+	dots7 = updateDots(dots7,coherence7)
+	// draw
+	ctx_sample2.clearRect(0,0,canvas_sample.width,canvas_sample.height);
+	clipCtx(ctx_sample2,canvas_sample2);
+		drawDots(dots7,ctx_sample2);
+	ctx_sample2.restore();
+
+	tick7 = window.requestAnimationFrame(drawMotionSample7);
+}
+
+function resetSample7() {
+	coherence7 = Math.random()*2-1;
+	dir7 = (coherence7>0) ? 1 : -1;
+	dots7.dir = (coherence7>0) ? 0 : Math.PI;
+	coherence7 = Math.abs(coherence7);
+	// Re-build the plot
+	drawPlot7();
+}
+
+function leftResponse(c,d) {return 0;}
+function rightResponse(c,d) {return 0;}
+function accumulateDDM(l,r,a) {return 0;}
+
+function buildPlot3() {
+	var mult = 2; // response multiplier (coherence>0) ? (coherence + randn() * noise)*mult : randn()*noise*mult
+	var noise = 1;
+	var data = {};
+	// build up the variables for the plot
+	// the evidence runs are just the L/R evidence 
+	data.eL = [0]; // evidence left
+	data.eR = [0];
+	data.E = [0]; // accumulated evidence
+	data.time = range(1,40);
+	for (var i=1; i<40; i++) {
+		var l = leftResponse(coherence7,dir7);
+		var r = rightResponse(coherence7,dir7);
+		var E = accumulateDDM(l,r,data.E[i-1]);
+		data.eL.push(l);
+		data.eR.push(r);
+		data.E.push(E);
+	}
+	return data;
+}
+
+function drawPlot7() {
+	var data2 = buildPlot3();
+	traceL = {
+		x: data2.time,
+		y: data2.eL,
+		mode:'line',
+		line:{color:'blue'},
+		type:'scatter',
+		name:'Left population response'
+	}
+	traceR = {
+		x: data2.time,
+		y: data2.eR,
+		mode:'line',
+		line:{color:'red'},
+		type:'scatter',
+		name:'Right population response'
+	}
+	traceM = {
+		x: data2.time,
+		y: data2.E,
+		mode:'line',
+		marker: {color:'black'},
+		type:'scatter',
+		name:'Accumulated evidence (left - right)'
+	}
+	layout.title = 'Accumulated evidence';
+	layout.xaxis.title = 'Time (ms)';
+	layout.xaxis.range = [0,40];
+	layout.yaxis.title = 'Evidence for RIGHT --------- Evidence for LEFT';
+	layout.yaxis.range = [-25,25];
+	Plotly.newPlot('plot3',[traceL,traceR, traceM],layout);
+}
 
 ///////////////////////////////////
 ////////// LOCAL CODE /////////////
 ///////////////////////////////////
 
 var done2 = false;
+var done6 = false;
+var done7 = false;
 
 function stopMotion() {
 	window.cancelAnimationFrame(tick3);
 	window.cancelAnimationFrame(tick4);
 	window.cancelAnimationFrame(tick5);
+	window.cancelAnimationFrame(tick7);
 }
 
 function run(i) {
@@ -297,6 +459,17 @@ function run(i) {
 		case 5:
 			drawMotionSample();
 			break;
+		case 6:
+			if (!done6) {
+				document.getElementById("continue").style.display="none";
+			}
+			break;
+		case 7:
+			drawMotionSample7();
+			if (!done7) {
+				document.getElementById("continue").style.display="none";
+			}
+			break;
  }
 }
 
@@ -304,6 +477,11 @@ function launch_local() {
 	katex.render("y(t)=y_{0}+v_{y}t+\\frac{1}{2}at^{2}",document.getElementById("katex1"),{displayMode:true});
 	katex.render("y(t)=y_{0}+v_{y}t+\\frac{1}{2}at^{2}",document.getElementById("katex2"),{displayMode:true});	
 	katex.render("difference(t)=left(t)-right(t)",document.getElementById("katex3"),{displayMode:true});	
+	katex.render("Response(coherence) = f(coherence) + \\epsilon",document.getElementById("katex4"),{displayMode:true});	
 	drawPlot1();
 	drawPlot2();
+	document.getElementById("bonus1").style.display="none";
+	document.getElementById("end6").style.display="none";
+	document.getElementById("end7").style.display="none";
+
 }
