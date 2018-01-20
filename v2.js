a680219263b56bfcc0c33c5da19ec69337a78c96
Dan Birman <danbirman@gmail.com>
2018-01-19 22:33:56 -0800

    Functional hover interaction and brain design

--- v2.js
+++ v2.js
@@ -1,18 +1,75 @@
-var modal = document.getElementById('myModal');
-var modal_content = document.getElementById('myContent');
 
 // When the user clicks anywhere on the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal || event.target == modal_content) {
-    	hide();
-    }
+// window.onclick = function(event) {
+//     if (event.target == modal || event.target == modal_content) {
+//     	hide();
+//     }
+// }
+
+function over(n) {
+	flicker(n);
+	document.getElementById("circ"+n).style.opacity = "0";
 }
 
-function disp() {
-	modal.style.display="block";
+function out(n) {
+	clearTimeout(ticks[n]);
+	elems[n].style.opacity = "1";
 }
 
-function hide() {
-    modal.style.display = "none";
+var modals = [];
+var areas = [cAbout,cCohcon,cLibet,cFBSear,cLearn,cSelfies];
+var ticks = [-1,-1,-1,-1,-1,-1];
+var opacity = [true,true,true,true,true,true];
+var elems = [];
+
+// Callbacks
+
+function area(n) {
+	areas[n]();
+}
+
+var cM_about = document.getElementById("cM_about");
+
+function cAbout(show) {
+	console.log('aboot');
+	if (show) {
+		cM_about.style.display="block";
+	} else {
+		cM_about.style.display="";
+	}
+}
+
+function cCohcon() {
 
 }
+
+function cLibet() {
+
+}
+
+function cFBSear() {
+
+}
+
+function cLearn() {
+
+}
+
+function cSelfies() {
+
+}
+
+function flicker(n) {
+	opacity[n] = !opacity[n];
+	elems[n].style.opacity = "" + Number(opacity[n]);
+	ticks[n] = setTimeout(function() {flicker(n);},150);
+}
+
+function init() {
+	for (var i=1;i<=6;i++) {
+		elems[i] = document.getElementById("circ"+i);
+		modals[i] = document.getElementById("modal"+i);
+	}
+}
+
+window.onload = init;
