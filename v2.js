a5b7845d833972f6429837428e606735456d4a7e
Dan Birman <danbirman@gmail.com>
2019-12-04 15:13:31 -0800

    Delay loading even more (don't even create the map until clicked)

--- v2.js
+++ v2.js
@@ -111,49 +111,60 @@ var layersLoaded = false;
 function checkLayersLoaded() {
 	if (!layersLoaded) {
 
-    // Add a layer showing the places.
-    var layer = {
-    	"source": {},
-    	"layout": {
-    		"icon-image": "{icon}-15",
-    		"icon-allow-overlap": true
-    	}
-    };
-    layer.id = "places";
-    layer.type = "symbol";
-    layer.source = {};
-    layer.source.type = "geojson";
-    layer.source.data = getData();
-    layer["layout"] = {
-    	"icon-image": "{icon}-15",
-    	"icon-allow-overlap": true
-    }
-    // layer.layout['icon-image'] = "{icon}-15";
-    // layer.layout['icon-allow-overlap'] = false;
 
-    map.addLayer(layer);
+		map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/outdoors-v10?optimize=true',
+			center: [-98.5795, 39.8283],
+			zoom: 1
+		});
 
-    // Create a popup, but don't add it to the map yet.
-    var popup = new mapboxgl.Popup({
-    	closeButton: false,
-    	closeOnClick: false
-    });
+		map.on('load', function() {
 
-    map.on('mouseenter', 'places', function(e) {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = 'default';
+	    // Add a layer showing the places.
+	    var layer = {
+	    	"source": {},
+	    	"layout": {
+	    		"icon-image": "{icon}-15",
+	    		"icon-allow-overlap": true
+	    	}
+	    };
+	    layer.id = "places";
+	    layer.type = "symbol";
+	    layer.source = {};
+	    layer.source.type = "geojson";
+	    layer.source.data = getData();
+	    layer["layout"] = {
+	    	"icon-image": "{icon}-15",
+	    	"icon-allow-overlap": true
+	    }
+	    // layer.layout['icon-image'] = "{icon}-15";
+	    // layer.layout['icon-allow-overlap'] = false;
 
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(e.features[0].geometry.coordinates)
-        .setHTML(e.features[0].properties.description)
-        .addTo(map);
-    });
+	    map.addLayer(layer);
 
-    map.on('mouseleave', 'places', function() {
-    	map.getCanvas().style.cursor = '';
-    	popup.remove();
-  	});
+	    // Create a popup, but don't add it to the map yet.
+	    var popup = new mapboxgl.Popup({
+	    	closeButton: false,
+	    	closeOnClick: false
+	    });
+
+	    map.on('mouseenter', 'places', function(e) {
+	        // Change the cursor style as a UI indicator.
+	        map.getCanvas().style.cursor = 'default';
+
+	        // Populate the popup and set its coordinates
+	        // based on the feature found.
+	        popup.setLngLat(e.features[0].geometry.coordinates)
+	        .setHTML(e.features[0].properties.description)
+	        .addTo(map);
+	    });
+
+	    map.on('mouseleave', 'places', function() {
+	    	map.getCanvas().style.cursor = '';
+	    	popup.remove();
+	  	}); 	
+		});
 	}
 }
 
@@ -175,17 +186,6 @@ function init() {
 		document.getElementById(""+i).style.cursor = "pointer";
 	}
 	mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuYmlybWFuOTk4IiwiYSI6ImNqNzl5YjhyeTA4ejYycXAzbTc4ZTFucjQifQ.o3KPRP5zwv01xg1WskZVRg';
-
-	map = new mapboxgl.Map({
-		container: 'map',
-		style: 'mapbox://styles/mapbox/outdoors-v10?optimize=true',
-		center: [-98.5795, 39.8283],
-		zoom: 1
-	});
-
-	// map.on('load', function() {
-
-	// });
 }
 
 window.onload = init;
